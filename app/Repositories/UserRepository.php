25a3cc3a45ee8fa8f1ab281097ec64e188239bc1
dmitriyPahomov <dmitriy.pahomov@newizze.com>
2022-11-18 16:15:50 +0300

    Task 2 made.

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserRepository
+{
+    public function __construct(
+        protected User $model,
+    ){}
+
+    public function findByName(string $name): User
+    {
+        return $this->model
+            ->select(['id', 'name', 'email'])
+            ->where('name', $name)
+            ->first();
+    }
+
+    public function create(array $data): User
+    {
+        return $this->model->create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
